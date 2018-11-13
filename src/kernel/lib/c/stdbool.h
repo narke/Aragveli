cb34f2f7cdb6f5f0444be71a753e5e54865f6416
Konstantin Tcholokachvili <konstantin.tcholokachvili@protonmail.com>
2018-11-13 10:29:48 +0100

    Moved some type definitions.

---
+++ src/kernel/lib/c/stdbool.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2018 Konstantin Tcholokachvili.
+ * All rights reserved.
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
+#pragma once
+
+typedef enum {false=0, true=1} bool;
---
+++ src/kernel/lib/c/stdint.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2018 Konstantin Tcholokachvili.
+ * All rights reserved.
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
+#pragma once
+
+typedef unsigned long long int	uint64_t;
+typedef unsigned long int	uint32_t;
+typedef unsigned short int	uint16_t;
+typedef unsigned char		uint8_t;
+
+typedef signed long long int    int64_t;
+typedef signed long int         int32_t;
+typedef signed short int        int16_t;
+typedef signed char             int8_t;
+
+typedef unsigned long		uintptr_t;
