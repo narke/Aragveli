5f58ae2e4aeb794e79f85c353f51cc780788f8c0
Konstantin Tcholokachvili <konstantin.tcholokachvili@protonmail.com>
2025-05-24 22:31:38 +0200

    feat: Addes source for paging stubs.

---
+++ src/kernel/arch/x86/paging-stubs.asm
@@ -0,0 +1,22 @@
+section .text
+
+global x86_load_page_directory
+x86_load_page_directory:
+    push ebp
+    mov ebp, esp
+    mov eax, [esp + 8]
+    mov cr3, eax
+    mov esp, ebp
+    pop ebp
+    ret
+
+global x86_enable_paging
+x86_enable_paging:
+    push ebp
+    mov ebp, esp
+    mov eax, cr0
+    or eax, 0x80000000
+    mov cr0, eax
+    mov esp, ebp
+    pop ebp
+    ret
