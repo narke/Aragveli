0828106d1646ce1074708927dff03a391799013e
Konstantin Tcholokachvili <konstantin.tcholokachvili@protonmail.com>
2026-07-12 22:17:01 +0200

    chore: Removed unused code in elf loader.

--- src/kernel/process/elf_loader.h
+++ src/kernel/process/elf_loader.h
@@ -16,7 +16,7 @@ typedef uint32_t Elf32_Addr;	// Unsigned address
 typedef uint32_t Elf32_Word;	// Unsigned int
 typedef int32_t  Elf32_Sword;	// Signed int
 
-# define ELF_NIDENT	16
+#define ELF_NIDENT	16
 
 typedef struct {
 	uint8_t		e_ident[ELF_NIDENT];
@@ -35,35 +35,16 @@ typedef struct {
 	Elf32_Half	e_shstrndx;
 } Elf32_Ehdr;
 
-enum Elf_Ident {
-	EI_MAG0		= 0, // 0x7F
-	EI_MAG1		= 1, // 'E'
-	EI_MAG2		= 2, // 'L'
-	EI_MAG3		= 3, // 'F'
-	EI_CLASS	= 4, // Architecture (32/64)
-	EI_DATA		= 5, // Byte Order
-	EI_VERSION	= 6, // ELF Version
-	EI_OSABI	= 7, // OS Specific
-	EI_ABIVERSION	= 8, // OS Specific
-	EI_PAD		= 9  // Padding
-};
+#define ELFMAG0	0x7F // e_ident[EI_MAG0]
+#define ELFMAG1	'E'  // e_ident[EI_MAG1]
+#define ELFMAG2	'L'  // e_ident[EI_MAG2]
+#define ELFMAG3	'F'  // e_ident[EI_MAG3]
 
-# define ELFMAG0	0x7F // e_ident[EI_MAG0]
-# define ELFMAG1	'E'  // e_ident[EI_MAG1]
-# define ELFMAG2	'L'  // e_ident[EI_MAG2]
-# define ELFMAG3	'F'  // e_ident[EI_MAG3]
+#define ELFDATA2LSB	(1)  // Little Endian
+#define ELFCLASS32	(1)  // 32-bit Architecture
 
-# define ELFDATA2LSB	(1)  // Little Endian
-# define ELFCLASS32	(1)  // 32-bit Architecture
-
-enum Elf_Type {
-	ET_NONE		= 0, // Unkown Type
-	ET_REL		= 1, // Relocatable File
-	ET_EXEC		= 2  // Executable File
-};
-
-# define EM_386		(3)  // x86 Machine Type
-# define EV_CURRENT	(1)  // ELF Current Version
+#define EM_386		(3)  // x86 Machine Type
+#define EV_CURRENT	(1)  // ELF Current Version
 
 bool elf_check_file(Elf32_Ehdr *hdr);
 bool elf_check_supported(Elf32_Ehdr *hdr);
